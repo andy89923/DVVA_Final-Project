7e817fe0572085a296eef0ad839686f72aa70585
scott306lr <scott306lr@gmail.com>
2023-01-09 00:13:07 +0800

    multi select done

---
+++ movis/src/pages/playground/index.tsx
@@ -0,0 +1,94 @@
+import { type NextPage } from "next";
+import Head from "next/head";
+import Link from "next/link";
+import { useState } from "react";
+import Navbar from "../../components/Navbar";
+import SubsetPicker from "../../components/SubsetPicker";
+import ZoomCard from "../../components/ZoomCard";
+
+import { api } from "../../utils/api";
+
+type Subset = {
+  name: string;
+  data: string[];
+  selected: boolean;
+};
+
+const testSubsets: Subset[] = [
+  {
+    name: "Test Subset 1",
+    data: ["a", "b", "c"],
+    selected: false,
+  },
+  {
+    name: "Test Subset 2",
+    data: ["d", "e", "f"],
+    selected: false,
+  },
+  {
+    name: "Test Subset 3",
+    data: ["g", "h", "i"],
+    selected: false,
+  },
+];
+
+const Playground: NextPage = () => {
+  const hello = api.example.hello.useQuery({ text: "from tRPC" });
+  const [subsets, setSubsets] = useState<Subset[]>(testSubsets);
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+      <Navbar />
+      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+        <div className="text-center font-extrabold tracking-tight text-white">
+          <h1 className="text-5xl sm:text-[5rem]">Welcome</h1>
+          <h2 className="text-3xl sm:text-[3rem]">
+            <span className="text-[hsl(295,32%,69%)]">to My Playground</span>
+          </h2>
+        </div>
+
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
+          <ZoomCard title="Link 1">
+            <Link
+              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+              href="https://create.t3.gg/en/usage/first-steps"
+              target="_blank"
+            >
+              <h3 className="text-2xl font-bold">First Steps →</h3>
+              <div className="text-lg">
+                Just the basics - Everything you need to know to set up your
+                database and authentication.
+              </div>
+            </Link>
+          </ZoomCard>
+          <ZoomCard title="Link 2">
+            <Link
+              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+              href="https://create.t3.gg/en/introduction"
+              target="_blank"
+            >
+              <h3 className="text-2xl font-bold">Documentation →</h3>
+              <div className="text-lg">
+                Learn more about Create T3 App, the libraries it uses, and how
+                to deploy it.
+              </div>
+            </Link>
+          </ZoomCard>
+        </div>
+      </div>
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-3 md:gap-8">
+        <div className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-lg text-white hover:bg-white/20">
+          <h3 className="text-2xl font-bold">Some</h3>
+        </div>
+        <div className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-lg text-white hover:bg-white/20">
+          <h3 className="text-2xl font-bold">Arbitary</h3>
+        </div>
+        <div className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-lg text-white hover:bg-white/20">
+          <SubsetPicker subsets={subsets} setSubsets={setSubsets} />
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Playground;
