6afa35e6966cfd4ce64172c2be0c801dd52b5130
scott306lr <scott306lr@gmail.com>
2023-01-10 04:47:37 +0800

    organizing playground

--- movis/src/pages/playground/index.tsx
+++ movis/src/pages/playground/index.tsx
@@ -96,63 +96,40 @@ const Playground: NextPage = () => {
               <span className="text-[hsl(295,32%,69%)]">to My Playground</span>
             </h2>
           </div>
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-            <ZoomCard title="Link 1">
-              <Link
-                className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-                href="https://create.t3.gg/en/usage/first-steps"
-                target="_blank"
-              >
-                <h3 className="text-2xl font-bold">First Steps →</h3>
-                <div className="text-lg">
-                  Just the basics - Everything you need to know to set up your
-                  database and authentication.
-                </div>
-              </Link>
-            </ZoomCard>
-            <ZoomCard title="Link 2">
-              <Link
-                className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-                href="https://create.t3.gg/en/introduction"
-                target="_blank"
-              >
-                <h3 className="text-2xl font-bold">Documentation →</h3>
-                <div className="text-lg">
-                  Learn more about Create T3 App, the libraries it uses, and how
-                  to deploy it.
-                </div>
-              </Link>
-            </ZoomCard>
-          </div>
         </div>
-        {movies != null && (
+        {movies != null ? (
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-3 md:gap-8">
-            <div className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-lg text-white hover:bg-white/20">
-              <h3 className="text-2xl font-bold">Create Subsets:</h3>
-              <button
-                className="rounded bg-[#2e026d] py-2 px-4 font-bold text-white hover:bg-[#15162c]"
-                onClick={() => {
-                  const newSubset: Subset = {
-                    name: "New Subset",
-                    data: movies,
-                    selected: false,
-                  };
-                  setSubsets([...subsets, newSubset]);
-                }}
-              >
-                Add a Subset With Full Movie Data
-              </button>
-            </div>
-            <div className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-lg text-white hover:bg-white/20">
-              <MyCombobox
-                data={movies}
-                selected={selected}
-                setSelected={setSelected}
-              />
-            </div>
-            <div className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-lg text-white hover:bg-white/20">
-              <SubsetPicker subsets={subsets} setSubsets={setSubsets} />
-            </div>
+            <ZoomCard title="Fuzzy Search Movies">
+              <div className="flex flex-col gap-4 rounded-xl bg-white/10 p-4 text-lg text-white hover:bg-white/20">
+                <MyCombobox
+                  data={movies}
+                  selected={selected}
+                  setSelected={setSelected}
+                />
+              </div>
+            </ZoomCard>
+            <ZoomCard title="Add New Subset">
+              <div className="flex flex-col gap-4 rounded-xl bg-white/10 p-4 text-lg text-white hover:bg-white/20">
+                <button
+                  className="rounded bg-[#2e026d] py-2 px-4 font-bold text-white hover:bg-[#15162c]"
+                  onClick={() => {
+                    const newSubset: Subset = {
+                      name: "New Subset",
+                      data: movies,
+                      selected: false,
+                    };
+                    setSubsets([...subsets, newSubset]);
+                  }}
+                >
+                  Add a Subset With Full Movie Data
+                </button>
+              </div>
+            </ZoomCard>
+            <ZoomCard title="Multi-select Subsets">
+              <div className="flex flex-col gap-4 rounded-xl bg-white/10 p-4 text-lg text-white hover:bg-white/20">
+                <SubsetPicker subsets={subsets} setSubsets={setSubsets} />
+              </div>
+            </ZoomCard>
 
             <ZoomCard title="Genre Count">
               <div className="flex flex-col gap-4 rounded-xl bg-white/90 p-4 text-lg text-black hover:bg-white/100">
@@ -160,6 +137,8 @@ const Playground: NextPage = () => {
               </div>
             </ZoomCard>
           </div>
+        ) : (
+          <h1 className="text-2xl font-bold text-white">Querying Data...</h1>
         )}
       </main>
     </>
