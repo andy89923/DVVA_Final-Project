7805673b4abdcda5b61fad9d52f778ff87b8bcda
scott306lr <scott306lr@gmail.com>
2023-01-09 22:32:20 +0800

    Added CTFland on navbar

--- movis/src/components/Navbar.tsx
+++ movis/src/components/Navbar.tsx
@@ -5,11 +5,15 @@ import Link from "next/link";
 import { api } from "../utils/api";
 
 // Navigation Element
-function Navigation(title: string, url: string) {
+const Navigation: React.FC<{ title: string; url: string }> = (props) => {
   return (
-      <Link href={url} className="font-bold hover:text-[hsl(295,32%,69%)]" key={title}>
-        {title}
-      </Link>
+    <Link
+      href={props.url}
+      className="font-bold hover:text-[hsl(295,32%,69%)]"
+      key={props.title}
+    >
+      {props.title}
+    </Link>
   );
 };
 
@@ -19,34 +23,23 @@ export default function Navbar() {
   );
 
   return (
-    <div className="h-20 flex flex-row justify-between items-center bg-black px-20 text-white sticky top-0">
+    <div className="sticky top-0 flex h-20 flex-row items-center justify-between bg-black px-20 text-white">
       <div className="page-nav flex flex-row items-center gap-6">
         <div className="brand mr-4">
           <Link href="/" className="text-2xl font-bold">
             Mo<span className="text-[hsl(280,100%,70%)]">Vis</span>
           </Link>
         </div>
-        {
-          [
-            ['Overview', '/overview'],
-            ['Comparison', '/compare'],
-            ['Crew Analysis', '/crew'],
-            ['Relation Analysis', '/relation'],
-            ['Playground', '/playground']
-          ].map(([title, url]) => 
-            Navigation(title!, url!)
-          ) 
-        }
+        <Navigation title="Overview" url="/overview" />
+        <Navigation title="Comparison" url="/compare" />
+        <Navigation title="Crew Analysis" url="/crew" />
+        <Navigation title="Relation Analysis" url="/relation" />
+        <Navigation title="Playground" url="/playground" />
+        <Navigation title="CTFangLand" url="/ctfangLand" />
       </div>
       <div className="user-settings flex flex-row gap-4">
-      {
-          [
-            ['Login', '/login'],
-            ['Register', '/register'],
-          ].map(([title, url]) => 
-            Navigation(title!, url!)
-          ) 
-      }
+        <Navigation title="Login" url="/login" />
+        <Navigation title="Register" url="/register" />
       </div>
     </div>
   );
